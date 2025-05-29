c3fd17ce92d63deb172c35e9904fdfacc9398545
sjlee-catthe <ee395000@gmail.com>
2025-05-29 22:25:43 +0900

    inital

---
+++ src/main/java/com/aivle0515/proj_05/Proj05Application.java
@@ -0,0 +1,13 @@
+package com.aivle0515.proj_05;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proj05Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proj05Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/aivle0515/proj_05/Proj05ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aivle0515.proj_05;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proj05ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
